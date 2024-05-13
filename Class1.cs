e8ab4c6249094be608e23169015b829cea7ddf7e
ljch27 <ljchoque27@gmail.com>
2024-05-12 22:45:20 -0300

    AnNum 2doParcial

---
+++ Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AN_2DO_PARCIAL
+{
+    public static class GaussSeidelSolver
+    {
+        public static double[] Solve(double[,] coefficients, double[] constants, double[] initialGuess, double tolerance = 1e-9, int maxIterations = 1000)
+        {
+            int n = constants.Length;
+            double[] x = new double[n];
+            double[] previous = new double[n];
+
+            Array.Copy(initialGuess, x, n);
+
+            int iterations = 0;
+            double error = double.MaxValue;
+
+            while (iterations < maxIterations && error > tolerance)
+            {
+                Array.Copy(x, previous, n);
+
+                for (int i = 0; i < n; i++)
+                {
+                    double sum = constants[i];
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (j != i)
+                        {
+                            sum -= coefficients[i, j] * x[j];
+                        }
+                    }
+                    x[i] = sum / coefficients[i, i];
+                }
+
+                error = CalculateError(x, previous);
+                iterations++;
+            }
+
+            if (iterations == maxIterations)
+            {
+                throw new InvalidOperationException("El método de Gauss-Seidel no convergió dentro del número máximo de iteraciones especificado.");
+            }
+
+            return x;
+        }
+
+        private static double CalculateError(double[] current, double[] previous)
+        {
+            double maxError = 0;
+            for (int i = 0; i < current.Length; i++)
+            {
+                double error = Math.Abs(current[i] - previous[i]);
+                if (error > maxError)
+                {
+                    maxError = error;
+                }
+            }
+            return maxError;
+        }
+    }
+}
---
+++ Solver.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AN_2DO_PARCIAL
+{
+    public partial class Solver : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Verificar si la solicitud es de tipo POST
+            if (Request.HttpMethod == "POST")
+            {
+                // Leer los datos JSON enviados desde el formulario HTML
+                string requestBody = new System.IO.StreamReader(Request.InputStream).ReadToEnd();
+                dynamic requestData = Newtonsoft.Json.JsonConvert.DeserializeObject(requestBody);
+
+                double[,] coefficients = requestData.coefficients.ToObject<double[,]>();
+                double[] constants = requestData.constants.ToObject<double[]>();
+
+                // Resolver el sistema de ecuaciones utilizando el método de Gauss-Seidel
+                double[] solution = GaussSeidelSolver.Solve(coefficients, constants, new double[constants.Length]);
+
+                // Enviar los resultados de vuelta como JSON
+                Response.ContentType = "application/json";
+                Response.Write(Newtonsoft.Json.JsonConvert.SerializeObject(solution));
+                Response.End();
+            }
+        }
+    }
+}
